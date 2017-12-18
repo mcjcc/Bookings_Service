3b91a4326bba6a0bf778865966e53e61e14131ec
Johnny Chen <mcjohnnychen@gmail.com>
2017-12-17 17:28:20 -0800

    Create csv data generator

---
+++ data/create-json-data.js
@@ -0,0 +1,141 @@
+/*
+This file when run will generate booking records
+
+ex:
+{
+  "listing_uuid": "829aa84a-4bba-411f-a4fb-38167a987cda",
+  "bookings": [
+    {
+      "user_uuid": "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
+      "listing_uuid": "829aa84a-4bba-411f-a4fb-38167a987cda",
+      "booking_uuid": "fdda765f-fc57-5604-a269-52a7df8164ec",
+      "PA_rating": "3",
+      "booking_start_date": "2017-11-28",
+      "booking_end_date": "2017-11-30",
+      "booking_cost_per_night": 100,
+      "booking_length": 3,
+      "booking_total_cost": 300,
+      "booking_date": '2016-01-24'
+    },
+    {
+      "user_uuid": "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
+      "listing_uuid": "829aa84a-4bba-411f-a4fb-38167a987cda",
+      "booking_uuid": "3bbcee75-cecc-5b56-8031-b6641c1ed1f1",
+      "PA_rating": 3,
+      "booking_start_date": "2017-12-10",
+      "booking_end_date": "2017-12-15",
+      "booking_cost_per_night": 100,
+      "booking_length": 6,
+      "booking_total_cost": 600,
+      "booking_date": '2016-05-24'
+    }
+  ]
+}
+*/
+
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+
+var outputFile = './bookings-json-data.json';
+
+// es6 styled IIFE
+console.time('create-listing');
+{
+  let wstream = fs.createWriteStream(outputFile);
+
+  // make 1mm objects
+  for (let i = 0; i < 1000000; i++) {
+
+    let listing_uuid = uuidv1();
+
+    let record_obj = {
+      listing_uuid: listing_uuid,
+      bookings: []
+    };
+
+    for (let j = 0; j < getRandomInt(1,10); j++) {
+      let user_uuid = uuidv1();
+      let booking_uuid = uuidv1();
+      let PA_rating = getRandomInt( 0, 6 );
+      let booking_start_date = formatDateToString( randomDate( new Date('1/1/17'), new Date() ) );
+      let booking_length = getRandomInt( 1, 32 );
+      let booking_end_date = formatDateToString( addDaysToDate( booking_start_date, booking_length ) );
+      let booking_cost_per_night = getRandomInt( 20, 501 );
+      let booking_total_cost = totalCost( booking_length, booking_cost_per_night );
+      let booking_date = formatDateToString( randomDate( new Date('1/1/16'), new Date('12/31/17') ) );
+
+      let booking_obj = {
+        user_uuid: user_uuid,
+        booking_uuid: booking_uuid,
+        listing_uuid: listing_uuid,
+        PA_rating: PA_rating,
+        booking_start_date: booking_start_date,
+        booking_end_date: booking_end_date,
+        booking_cost_per_night: booking_cost_per_night,
+        booking_length: booking_length,
+        booking_total_cost: booking_total_cost,
+        booking_date: booking_date
+      };
+
+      record_obj.bookings.push(booking_obj);
+    }
+
+    let string = JSON.stringify(record_obj) + '\n';
+    wstream.write(string);
+  }
+
+  wstream.end(() => {
+    console.log('Bookings data has been created!');
+    console.timeEnd('create-listing');
+  });
+}
+
+// get a random integer between min and max
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+// calculate total cost of a booking (number of days * booking cost per night)
+function totalCost(number_of_days, booking_cost_per_night) {
+  return number_of_days * booking_cost_per_night;
+}
+
+// creates a random date between a date
+// ex: randomDate( new Date('1/1/17'), new Date() )
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+// takes a date and adds a specified amount of days to that date and returns the new date
+// addDaysToDate( new Date('1/1/17'), 7 )
+function addDaysToDate(date, days) {
+  var newDate = new Date(date);
+  return new Date(newDate.setDate(newDate.getDate() + days));
+}
+
+// takes a date object and formats to a string in this form 'YYYY-MM-DD'
+function formatDateToString(date) {
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  return `${year}-${month}-${day}`;
+}
+
+// takes two dates and calculates the amount of days between them
+function daysBetween(date1, date2) {
+  //Get 1 day in milliseconds
+  var one_day=1000*60*60*24;
+
+  // Convert both dates to milliseconds
+  var date1_ms = date1.getTime();
+  var date2_ms = date2.getTime();
+
+  // Calculate the difference in milliseconds
+  var difference_ms = date2_ms - date1_ms;
+
+  // Convert back to days and return
+  return Math.round(difference_ms/one_day);
+}
--- db/index.js
+++ db/index.js
@@ -7,17 +7,19 @@ client.connect()
   .then(function() {
     const query = "CREATE KEYSPACE IF NOT EXISTS bookings_db WITH replication =" +
                   "{ 'class' : 'SimpleStrategy', 'replication_factor': '3'}";
+    console.log('Keyspace: bookings_db created!');
     return client.execute(query);
   })
   .then(function() {
     const query = "CREATE TYPE IF NOT EXISTS bookings_db.booking" +
-                  "(user_uuid UUID, listing_uuid UUID, booking_uuid UUID, PA_rating int, booking_start_date text, booking_end_date text, booking_cost_per_night int, booking_length int, booking_total_cost int)";
+                  "(user_uuid UUID, listing_uuid UUID, booking_uuid UUID, PA_rating int, booking_start_date text, booking_end_date text, booking_cost_per_night int, booking_length int, booking_total_cost int, booking_date text)";
+    console.log('User Defined Type: bookings_db.booking created!');
     return client.execute(query);
   })
   .then(function() {
     const query = "CREATE TABLE IF NOT EXISTS bookings_db.bookings_table" +
                   "(listing_uuid UUID PRIMARY KEY, bookings list<FROZEN<booking>>)"
-
+    console.log('Table: bookings_db.bookings_table created!');
     return client.execute(query);
   })
   .then(function () {
@@ -43,7 +45,8 @@ CREATE TYPE IF NOT EXISTS bookings_db.booking (
   booking_end_date text,
   booking_cost_per_night int,
   booking_length int,
-  booking_total_cost int
+  booking_total_cost int,
+  booking_date text
 );
 
 CREATE TABLE IF NOT EXISTS bookings_db.bookings_table (
@@ -51,6 +54,7 @@ CREATE TABLE IF NOT EXISTS bookings_db.bookings_table (
   bookings list<FROZEN<booking>>
 );
 
+1 query:
 INSERT INTO bookings_db.bookings_table JSON '{
   "listing_uuid": "829aa84a-4bba-411f-a4fb-38167a987cda",
   "bookings": [
@@ -58,45 +62,57 @@ INSERT INTO bookings_db.bookings_table JSON '{
       "user_uuid": "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
       "listing_uuid": "110ec58a-a0f2-4ac4-8393-c866d813b8d1",
       "booking_uuid": "fdda765f-fc57-5604-a269-52a7df8164ec",
-      "PA_rating": "3",
+      "PA_rating": 3,
       "booking_start_date": "2017-11-28",
       "booking_end_date": "2017-11-30",
       "booking_cost_per_night": 100,
       "booking_length": 3,
-      "booking_total_cost": 300
+      "booking_total_cost": 300,
+      "booking_date": "2017-10-02"
     },
     {
       "user_uuid": "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
       "listing_uuid": "110ec58a-a0f2-4ac4-8393-c866d813b8d1",
       "booking_uuid": "3bbcee75-cecc-5b56-8031-b6641c1ed1f1",
-      "PA_rating": "3",
+      "PA_rating": 3,
       "booking_start_date": "2017-12-10",
       "booking_end_date": "2017-12-15",
       "booking_cost_per_night": 100,
       "booking_length": 6,
-      "booking_total_cost": 600
+      "booking_total_cost": 600,
+      "booking_date": "2017-10-04"
     }
   ]
 }';
 
-----------------------------------------------------
-
-CREATE TABLE bookings_db.bookings (
-  id UUID
-  user_uuid UUID
-  listing_uuid UUID
-  booking_uuid UUID
-  PA_rating int
-  booking_start_date text
-  booking_end_date text
-  booking_cost_per_night int
-  booking_length int
-  booking_total_cost int
-  PRIMARY KEY(id, listing_uuid, user_uuid)
-);
-
-// store data next
-
-
---
+another query:
+INSERT INTO bookings_db.bookings_table JSON '{
+  "listing_uuid": "999aa84a-4bba-411f-a4fb-38167a987cda",
+  "bookings": [
+    {
+      "user_uuid": "9994fb90-12c4-11e1-840d-7b25c5ee775a",
+      "listing_uuid": "999ec58a-a0f2-4ac4-8393-c866d813b8d1",
+      "booking_uuid": "999a765f-fc57-5604-a269-52a7df8164ec",
+      "PA_rating": 5,
+      "booking_start_date": "2017-11-28",
+      "booking_end_date": "2017-11-30",
+      "booking_cost_per_night": 100,
+      "booking_length": 3,
+      "booking_total_cost": 300,
+      "booking_date": "2017-10-02"
+    },
+    {
+      "user_uuid": "7774fb90-12c4-11e1-840d-7b25c5ee775a",
+      "listing_uuid": "999ec58a-a0f2-4ac4-8393-c866d813b8d1",
+      "booking_uuid": "777cee75-cecc-5b56-8031-b6641c1ed1f1",
+      "PA_rating": 5,
+      "booking_start_date": "2017-12-10",
+      "booking_end_date": "2017-12-15",
+      "booking_cost_per_night": 100,
+      "booking_length": 6,
+      "booking_total_cost": 600,
+      "booking_date": "2017-10-04"
+    }
+  ]
+}';
 */
