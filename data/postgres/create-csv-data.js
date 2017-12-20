50ba8a405a57ef878fca8c5936ccabd82a96f33c
Johnny Chen <mcjohnnychen@gmail.com>
2017-12-20 11:48:21 -0800

    Refactor bookings data generator so there aren't any overlapping dates

--- data/postgres/create-csv-data.js
+++ data/postgres/create-csv-data.js
@@ -20,13 +20,10 @@ console.time('create-listing');
 {
   let wstream = fs.createWriteStream(outputFile);
 
-  // make a lot of records
-  let records_amount = 10000000;
-
-  // generate 10% listings_uuid
-  let amount_of_listings = records_amount / 10;
+  // generate 1 mil listings_uuid
+  let listings_amount = 1000000;
   let listings_array = [];
-  for (let i = 0; i < amount_of_listings; i++) {
+  for (let i = 0; i < listings_amount; i++) {
     if (i % 100000 === 0) {
 		  // console log every 100k to show progress
       console.log('Another 100k listings generated! ', i);
@@ -34,10 +31,10 @@ console.time('create-listing');
     listings_array.push([chance.guid(), getRandomInt(0, 6)]);
   }
 
-  // generate 10% user_uuids
-  let amount_of_users = records_amount / 10;
+  // generate 1mil user_uuids
+  let users_amount = 1000000;
   let users_array = [];
-  for (let i = 0; i < amount_of_users; i++) {
+  for (let i = 0; i < users_amount; i++) {
     if (i % 100000 === 0) {
 		  // console log every 100k to show progress
       console.log('Another 100k users generated! ', i);
@@ -45,36 +42,43 @@ console.time('create-listing');
       users_array.push(chance.guid());
   }
 
-  for (let i = 0; i < records_amount; i++) {
+
+  let number_of_bookings_per_listing = 10;
+  // lets generate 10 booking records per listing to make 10 million records
+  for (let i = 0; i < listings_array.length; i++) {
+
     if (i % 100000 === 0) {
-		  // console log every 100k to show progress
-      console.log('Another 100k bookings record generated! ', i);
+      console.log('Amount of bookings record generated! ', i * number_of_bookings_per_listing);
     }
 
-    let record_str = '';
+    for (let j = 0; j < number_of_bookings_per_listing; j++) {
+      let record_str = '';
 
-    let listing = listings_array[getRandomInt(0, amount_of_listings)];
+      let listing = listings_array[i];
 
-    let booking_uuid = chance.guid();
-    let listing_uuid = listing[0];
-    let user_uuid = users_array[getRandomInt(0, amount_of_users)];
-    let PA_rating = listing[1];
-    let booking_start_date = formatDateToString( randomDate( new Date('1/1/2018'), new Date('12/31/2018') ) );
-    let booking_length = getRandomInt( 1, 32 );
-    let booking_end_date = formatDateToString( addDaysToDate( booking_start_date, booking_length ) );
-    let booking_cost_per_night = getRandomInt( 20, 501 );
-    let booking_total_cost = totalCost( booking_length, booking_cost_per_night );
-    let booking_date = formatDateToString( randomDate( new Date('1/1/2017'), new Date('12/31/2017') ) );
+      let booking_uuid = chance.guid();
+      let listing_uuid = listing[0];
+      let user_uuid = users_array[getRandomInt(0, users_amount)];
+      let PA_rating = listing[1];
+      let booking_start_date = formatDateToString( new Date(`${j+1}/1/2018`) );
+      let booking_length = getRandomInt( 3, 20 );
+      let booking_end_date = formatDateToString( addDaysToDate( booking_start_date, booking_length ) );
+      let booking_cost_per_night = getRandomInt( 20, 501 );
+      let booking_total_cost = totalCost( booking_length, booking_cost_per_night );
+      let booking_date = formatDateToString( subtractDaysToDate( booking_start_date, booking_length ) );
 
-    record_str = `${booking_uuid},${listing_uuid},${user_uuid},${PA_rating},'${booking_start_date}','${booking_end_date}',${booking_length},${booking_cost_per_night},${booking_total_cost},'${booking_date}'\r\n`;
-    wstream.write(record_str);
-  }
+      record_str = `${booking_uuid},${listing_uuid},${user_uuid},${PA_rating},'${booking_start_date}','${booking_end_date}',${booking_length},${booking_cost_per_night},${booking_total_cost},'${booking_date}'\r\n`;
+      wstream.write(record_str);
+    }
+
+  } // for listings_array loop
 
   wstream.end(() => {
     console.log('Bookings data has been created!');
     console.timeEnd('create-listing');
   });
 }
+
 
 // get a random integer between min and max
 function getRandomInt(min, max) {
@@ -99,6 +103,13 @@ function randomDate(start, end) {
 function addDaysToDate(date, days) {
   var newDate = new Date(date);
   return new Date(newDate.setDate(newDate.getDate() + days));
+}
+
+// takes a date and subracts a specified amount of days to that date and returns the new date
+// subractDaysToDate( new Date('1/1/17'), 7 )
+function subtractDaysToDate(date, days) {
+  var newDate = new Date(date);
+  return new Date(newDate.setDate(newDate.getDate() - days));
 }
 
 // takes a date object and formats to a string in this form 'YYYY-MM-DD'
