caa56b9ae4e9a44cb22031554b6e3af4d8046b27
Johnny Chen <mcjohnnychen@gmail.com>
2017-12-20 16:25:31 -0800

    Set up really basic queue with AWS

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const aws = require('aws-sdk');
+const path = require('path');
+
+// Load your AWS credentials and try to instantiate the object.
+let aws_config_path = path.join(__dirname, '../config/AWS.json');
+aws.config.loadFromPath(aws_config_path);
+
+
+let sqs = new aws.SQS();
 
 const Booking = require('../db/booking');
 
@@ -10,6 +19,10 @@ app.listen(port);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('Thesis Bookings API');
+})
 
 
 // get availability for a specific listing
@@ -50,3 +63,23 @@ app.post('/bookings/book/:listing_uuid', (req, res) => {
 // SQS msg bus to Events Service
 
 // SQS msg bus to Inventory Service
+// queue_url: https://sqs.us-west-1.amazonaws.com/732263591912/update_inventory_queue
+
+app.get('/update-inventory-service', (req, res) => {
+    let update_inventory_queue_url = 'https://sqs.us-west-1.amazonaws.com/732263591912/update_inventory_queue';
+
+    var params = {
+        MessageBody: 'Hello world!',
+        QueueUrl: update_inventory_queue_url,
+        DelaySeconds: 0
+    };
+
+    sqs.sendMessage(params, function(err, data) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
